b4676635e20dab51ed4854232b9bbb6bd83fec98
waterNeeeeeed <8gt8@163.com>
2017-05-06 12:24:48 +0800

    加入JEXL解析工资公式

---
+++ src/main/java/com/runfeng/hibernate/SalaryEntity/SalaryColumnMarks.java
@@ -0,0 +1,18 @@
+package com.runfeng.hibernate.SalaryEntity;
+
+/**
+ * Created by lenovo on 2017/5/6.
+ */
+/*enum OperateType{
+    PLUS_TYPE("+"),
+    MINUS_TYPE("-");
+    private OperateType(String operate){
+
+    }
+}*/
+public class SalaryColumnMarks {
+    private String columnId;
+    private String columnMark;
+    private String columnName;
+    private String OperateType;
+}
---
+++ src/main/java/com/runfeng/hibernate/SalaryEntity/SalaryTablePersonId.java
@@ -0,0 +1,18 @@
+package com.runfeng.hibernate.SalaryEntity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by lenovo on 2017/5/6.
+ */
+public class SalaryTablePersonId {
+    private int eid;
+    private String name;
+    /*<columnId,columnExpression>*/
+    private Map<String, String> columnIdAndExpr
+            = new HashMap<>();
+
+}
